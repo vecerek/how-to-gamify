92435c65512200ecdd0d3df45e06fb4d7e4e2118
Attila Večerek <avecerek@zendesk.com>
2019-04-21 13:08:57 +0200

    Move JSON schemas to the schema folder

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import path from 'path';
 import jsonRefParser from 'json-schema-ref-parser';
 
 const ROOT_PATH = path.resolve(__dirname, '..');
-const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+const SCHEMAS_PATH = path.resolve(ROOT_PATH, 'src', 'schemas');
 
 (async function() {
   try {
-    const frameworksPath = path.resolve(SRC_PATH, 'frameworks.json');
+    const frameworksPath = path.resolve(SCHEMAS_PATH, 'frameworks.json');
     const frameworks = await jsonRefParser.dereference(frameworksPath);
 
     console.log(frameworks.definitions['1'].features);

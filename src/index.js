a6c938fe78cdedabf6262067eaee7e5f70d0b15b
Attila Večerek <avecerek@zendesk.com>
2019-04-22 11:17:41 +0200

    Move features and frameworks to a top level data folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import path from 'path';
 import jsonRefParser from 'json-schema-ref-parser';
 
-const ROOT_PATH = path.resolve(__dirname, '..');
-const SCHEMAS_PATH = path.resolve(ROOT_PATH, 'src', 'schemas');
+const DATA_PATH = path.resolve(__dirname, '..', 'data');
 
-(async function() {
+(async function () {
   try {
-    const frameworksPath = path.resolve(SCHEMAS_PATH, 'frameworks.json');
+    const frameworksPath = path.resolve(DATA_PATH, 'frameworks.json');
     const frameworks = await jsonRefParser.dereference(frameworksPath);
 
     console.log(frameworks.definitions['1'].features);
   } catch (e) {
     console.log(e);
   }
-
-})();
+}());

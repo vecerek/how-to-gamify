d92aca232a46146128d09ac9f9febbcb319ee63c
Attila Večerek <avecerek@zendesk.com>
2019-04-22 11:19:57 +0200

    Fix linting error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 import path from 'path';
 import jsonRefParser from 'json-schema-ref-parser';
 
 const DATA_PATH = path.resolve(__dirname, '..', 'data');
 
-(async function () {
+(async function run() {
   try {
     const frameworksPath = path.resolve(DATA_PATH, 'frameworks.json');
     const frameworks = await jsonRefParser.dereference(frameworksPath);

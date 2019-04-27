196e38d5605e2a47022fbfbf298850d186a7f459
Attila Večerek <avecerek@zendesk.com>
2019-04-27 15:08:24 +0200

    Updates

---
+++ precache-manifest.9e5afb4ac86bee58a0f5223e465cabe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac087ca61a825967c26fa65124991ad",
+    "url": "/how-to-gamify/index.html"
+  },
+  {
+    "revision": "476f6059016d4e6949f5",
+    "url": "/how-to-gamify/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/how-to-gamify/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "476f6059016d4e6949f5",
+    "url": "/how-to-gamify/static/js/main.2193ccab.chunk.js"
+  },
+  {
+    "revision": "90eddf1e5f0d5cbed57e",
+    "url": "/how-to-gamify/static/js/runtime~main.fc294eef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/how-to-gamify/static/media/logo.5d5d9eef.svg"
+  }
+]);

ad2d70b2a938410be9fa7fbb0bcf276dcbf22a60
Attila Večerek <avecerek@zendesk.com>
2019-04-29 18:34:57 +0200

    Add dummy route for the get started component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router';
 import Home from '../Home';
+import GetStarted from '../GetStarted';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/get-started" component={GetStarted} />
     </Switch>
   );
 }

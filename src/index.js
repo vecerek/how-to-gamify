5606d61f76e3e4a7f4738aac83aed15a6010cd25
Attila Večerek <avecerek@zendesk.com>
2019-04-29 13:58:25 +0200

    Integrate and configure Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const middleware = [
+  thunkMiddleware,
+];
+
+if (process.env.NODE_ENV === 'development') {
+  const { createLogger } = require('redux-logger');
+
+  middleware.push(
+    createLogger({ collapsed: true })
+  );
+}
+
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(...middleware))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.register();

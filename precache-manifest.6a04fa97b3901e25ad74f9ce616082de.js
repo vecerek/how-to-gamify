9744282d420366a6ec71cb36aebfdfadc292efee
Attila Večerek <avecerek@zendesk.com>
2019-04-29 19:05:20 +0200

    Updates

---
+++ precache-manifest.6a04fa97b3901e25ad74f9ce616082de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ea7fc1184b8c81462822451fd7cecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549af9a389b8ab877f7a",
+    "url": "/static/css/main.22a407fc.chunk.css"
+  },
+  {
+    "revision": "97b7c03a8829c5f5b8a3",
+    "url": "/static/js/2.7c03bd01.chunk.js"
+  },
+  {
+    "revision": "549af9a389b8ab877f7a",
+    "url": "/static/js/main.9b9d3f13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5727634091c05cb6a26b4d2c350686f2.js"
+  "/precache-manifest.6a04fa97b3901e25ad74f9ce616082de.js"
 );
 
 self.addEventListener('message', (event) => {

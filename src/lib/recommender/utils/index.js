9660aa69fbaa443c1ab3e75da4a9ac0afd33a1c3
Attila Večerek <avecerek@zendesk.com>
2019-04-30 16:58:51 +0200

    Fix production data loading

--- src/lib/recommender/utils/index.js
+++ src/lib/recommender/utils/index.js
@@ -3,12 +3,15 @@ import jsonRefParser from 'json-schema-ref-parser';
 
 const DATA_PATH = path.resolve(__dirname, '..', 'data');
 
-const loadData = file => {
+const loadData = async file => {
   const dataPath = path.resolve(DATA_PATH, `${file}.json`);
 
-  return process.env.NODE_ENV === 'test'
-    ? jsonRefParser.dereference(dataPath)
-    : import(dataPath);
+  if (process.env.NODE_ENV === 'test') {
+    return await jsonRefParser.dereference(dataPath);
+  }
+
+  const data = (await import(`../data/${file}.json`)).default;
+  return Object.values(data);
 };
 
 export default loadData;

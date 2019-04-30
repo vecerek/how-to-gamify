a8fd70d53642e6a244e38463ab365f631e67c3a1
Attila Večerek <avecerek@zendesk.com>
2019-04-30 16:59:25 +0200

    Extend Recommender class with data loading static methods

--- src/lib/recommender/index.js
+++ src/lib/recommender/index.js
@@ -1,10 +1,19 @@
 import Framework from './framework';
-import ReferenceFramework from './framework/reference-framework'
+import ReferenceFramework from './framework/reference-framework';
+import loadData from './utils';
 
 export default class Recommender {
   constructor(frameworks, reference) {
     this.reference = ReferenceFramework.create(reference);
     this.frameworks = frameworks.map(f => Framework.create(f));
+  }
+
+  static async loadFeatures() {
+    return await loadData('features');
+  }
+
+  static async loadFrameworks() {
+    return await loadData('frameworks');
   }
 
   get results() {

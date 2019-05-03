457286442c2732a42c18d480b5a128c44b09af05
Attila Večerek <avecerek@zendesk.com>
2019-05-03 08:58:50 +0200

    Extract the availability indicator out into its own (shared) component

--- src/components/Results/components/DetailsDrawer/components/Features/index.js
+++ src/components/Results/components/DetailsDrawer/components/Features/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
-import WarningIcon from '@material-ui/icons/Warning';
-import StarIcon from '@material-ui/icons/Star';
-import CheckIcon from '@material-ui/icons/Check';
-import ClearIcon from '@material-ui/icons/Clear';
-import red from '@material-ui/core/colors/red';
-import yellow from '@material-ui/core/colors/yellow';
-import blue from '@material-ui/core/colors/blue';
-import green from '@material-ui/core/colors/green';
+import AvailabilityIndicator from '../../../../../shared/AvailabilityIndicator';
 import { titleize } from '../../../../../../lib/formatting';
 
 const styles = theme => ({
@@ -29,22 +21,6 @@ const styles = theme => ({
     display: 'flex',
     alignItems: 'center',
   },
-  icon: {
-    fontSize: '18px',
-    marginRight: '5px',
-  },
-  match: {
-    color: green[500],
-  },
-  missing: {
-    color: red[500],
-  },
-  partial: {
-    color: yellow[500],
-  },
-  extra: {
-    color: blue[500],
-  }
 });
 
 const getHeading = variant => {
@@ -62,31 +38,15 @@ const getHeading = variant => {
   }
 };
 
-const getIcon = variant => {
-  switch(variant) {
-    case 'match':
-      return CheckIcon;
-    case 'missing':
-      return ClearIcon;
-    case 'partial':
-      return WarningIcon;
-    case 'extra':
-      return StarIcon;
-    default:
-      throw new Error('Not implemented');
-  }
-}
-
 const Features = ({ classes, features, variant, count }) => {
   if (features.length === 0) return null;
 
   const heading = getHeading(variant);
-  const Icon = getIcon(variant);
 
   return (
     <div id={`features-${variant}`} className={classes.col}>
       <Typography className={classes.iconHeading} variant="subtitle1" gutterBottom>
-        <Icon className={classnames(classes.icon, classes[variant])} />
+        <AvailabilityIndicator variant={variant} />
         {`${heading} (${features.length}/${count})`}
       </Typography>
       {features.map(feature =>
---
+++ src/components/shared/AvailabilityIndicator/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import WarningIcon from '@material-ui/icons/Warning';
+import StarIcon from '@material-ui/icons/Star';
+import CheckIcon from '@material-ui/icons/Check';
+import ClearIcon from '@material-ui/icons/Clear';
+import red from '@material-ui/core/colors/red';
+import yellow from '@material-ui/core/colors/yellow';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+
+const styles = {
+  icon: {
+    fontSize: '18px',
+    marginRight: '5px',
+  },
+  match: {
+    color: green[500],
+  },
+  missing: {
+    color: red[500],
+  },
+  partial: {
+    color: yellow[500],
+  },
+  extra: {
+    color: blue[500],
+  }
+};
+
+const getIcon = variant => {
+  switch(variant) {
+    case 'match':
+      return CheckIcon;
+    case 'missing':
+      return ClearIcon;
+    case 'partial':
+      return WarningIcon;
+    case 'extra':
+      return StarIcon;
+    default:
+      throw new Error('Not implemented');
+  }
+}
+
+const AvailabilityIndicator = ({ classes, variant }) => {
+  const Icon = getIcon(variant);
+
+  return <Icon className={classnames(classes.icon, classes[variant])} />
+};
+
+AvailabilityIndicator.propTypes = {
+  classes: PropTypes.object.isRequired,
+  variant: PropTypes.oneOf(['match', 'missing', 'partial', 'extra']).isRequired,
+};
+
+export default withStyles(styles)(AvailabilityIndicator);
